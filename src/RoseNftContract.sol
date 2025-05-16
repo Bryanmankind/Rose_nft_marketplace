2a1cfca7df2839cebb1a5738685a36900555a456
Bryanmankind <shegeemankind@gmail.com>
2025-05-16 13:27:19 +0100

    update

--- src/RoseNftContract.sol
+++ src/RoseNftContract.sol
@@ -9,25 +9,72 @@ import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 contract RoseNft is ERC721, Ownable{
 
     // @notice Counter used for generating unique token ids.
-    uint256 private _currentTokenId;
+    uint256 private _currentBirdId;
+    uint256 public BirdId;
+    uint256 public currentNumOfBirdGenBreed;
 
-    // keep track of the part of the bird
-    struct BirdColors {
-        string Body;
-        string wings;
-        string eyes;
-        string tail;
-        string legs;
-        string beak;
+    mapping(address => uint256) public userBirdIdBalance; 
+    mapping(uint256 => address) public ownerOfBirdNft;
+
+    uint256 constant MAX_BIRD_GEN_BREED = 10;
+
+
+    error birdGenBreedExceeded();
+
+    // keep track of the bird gen structure 
+    struct Bird {
+        uint genes;
+        uint64 birthTime;
+        uint32 mumId;
+        uint32 dadId;
+        uint32 readyTime;
+        uint16 generation;
     }
 
-    // svg image of the first Bird 
+    Bird[] public birdList;
 
-    string Bird_default = 
-       '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="1024" height="1024" viewBox="0 0 1024 1024">';
+    event Birth(address owner, uint256 kittenId, uint256 mumId, uint256 dadId, uint256 genes);
 
     constructor () ERC721 ("BirdNft", "Brd") Ownable(msg.sender){}
 
     function mint () public returns (bool) {
     }
+
+    // This function returns the number of Bird Nft owned by an address
+    function balanceOf(address _owner) public view override returns (uint256 balance) {
+        balance = userBirdIdBalance[_owner];
+    }
+
+    // This function returns the owner of the Bird nft 
+    function ownerOf(uint256 _birdId) public view override returns (address owner) {
+        owner = ownerOfBirdNft[_birdId];
+    }
+
+    function createBirdNftOrigin (uint256 _birdGenes) public onlyOwner returns (uint256) {
+        if (currentNumOfBirdGenBreed > MAX_BIRD_GEN_BREED) {
+            revert birdGenBreedExceeded();
+        }
+
+        currentNumOfBirdGenBreed++;
+
+        return _createBirdNft (0, 0, 0, _birdGenes, msg.sender);
+    }
+
+    function _createBirdNft (uint256 dadBirdId, uint256 mumBirdId, uint256 generation, uint256 genes, address owner) internal returns (uint256) {
+         Bird memory _birdGen01 = Bird ({
+            genes: genes,
+            birthTime: uint64(block.timestamp),
+            mumId: uint32(mumBirdId),
+            dadId: uint32(dadBirdId),
+            readyTime: uint32(block.timestamp),
+            generation: uint16(generation)
+        });
+             birdList.push(_birdGen01);
+
+            uint newBirdNftId = birdList.length-1;
+            ownerOfBirdNft[newBirdNftId]= owner;
+             _transfer(address(0), owner, newBirdNftId);
+            emit Birth(owner, newBirdNftId, mumBirdId, dadBirdId, genes);
+            return newBirdNftId;
+    }   
 }
